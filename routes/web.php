b510b11306f298df0d9fac204130d933e7a3946f
dautiep <dautiep123@gmail.com>
2022-11-30 22:37:54 +0700

    change permission

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
 
     // groups
     Route::group(['prefix' => 'groups', 'as' => 'groups.'], function () {
-        Route::get('/', 'GroupController@list')->name('list');
+        Route::get('/', 'GroupController@list')->name('list')->middleware('role:services.list');
         Route::get('create', 'GroupController@create')->name('create');
         Route::post('state', 'GroupController@state')->name('state');
         Route::post('/delete', 'GroupController@delete')->name('delete');
