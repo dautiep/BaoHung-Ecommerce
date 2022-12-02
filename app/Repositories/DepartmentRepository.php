d234f358ced961beb72580189e1d5bdeffc30694
TaylorPMT <thien.phamminhstu@gmail.com>
2022-12-02 15:14:10 +0700

    add repository department

---
+++ app/Repositories/DepartmentRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\DepartmentRepositoryInterface;
+
+class DepartmentRepository extends BaseRepository implements DepartmentRepositoryInterface
+{
+}
