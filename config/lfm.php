ef8ee213df55d60614fe29a1f9059bbad8de3098
TaylorPMT <thien.phamminhstu@gmail.com>
2022-12-07 22:44:35 +0700

    add file config upload file

--- config/lfm.php
+++ config/lfm.php
@@ -58,8 +58,20 @@ return [
                 'image/pjpeg',
                 'image/png',
                 'image/gif',
+                'image/svg+xml',
                 'application/pdf',
                 'text/plain',
+                'application/vnd.ms-excel',
+                'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                'application/docx',
+                'application/doc',
+                'application/pptx',
+                'application/ppt',
+                'application/xlsx',
+                'application/xls',
+                'application/msword',
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'application/vnd.openxmlformats-officedocument.presentationml.presentation'
             ],
         ],
         'image' => [
@@ -160,6 +172,21 @@ return [
         'png'  => 'PNG Image',
         'ppt'  => 'Microsoft PowerPoint',
         'pptx' => 'Microsoft PowerPoint',
+    ],
+
+    'file_icon_array'          => [
+        'pdf'  => 'fa-file-pdf-o',
+        'doc'  => 'fa-file-word-o',
+        'docx' => 'fa-file-word-o',
+        'xls'  => 'fa-file-excel-o',
+        'xlsx' => 'fa-file-excel-o',
+        'zip'  => 'fa-file-archive-o',
+        'gif'  => 'fa-file-image-o',
+        'jpg'  => 'fa-file-image-o',
+        'jpeg' => 'fa-file-image-o',
+        'png'  => 'fa-file-image-o',
+        'ppt'  => 'fa-file-powerpoint-o',
+        'pptx' => 'fa-file-powerpoint-o',
     ],
 
     /*
