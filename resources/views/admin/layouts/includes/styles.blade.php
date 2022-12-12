352a7036bafcf087eee5773d9a9bbb6c4dbb252c
TaylorPMT <thien.phamminhstu@gmail.com>
2022-12-12 17:43:26 +0700

    fix errors fonts

--- resources/views/admin/layouts/includes/styles.blade.php
+++ resources/views/admin/layouts/includes/styles.blade.php
@@ -1,14 +1,16 @@
 <!-- Google Font: Source Sans Pro -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<link rel="stylesheet" href="{{ asset('admin/fonts/Source_Sans_Pro/main.css') }}">
 <!-- Font Awesome -->
 <link rel="stylesheet" href="{{ asset('admin/admin-lte/plugins/fontawesome-free/css/all.min.css') }}">
 <!-- Tempusdominus Bootstrap 4 -->
-<link rel="stylesheet" href="{{ asset('admin/admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+<link rel="stylesheet"
+    href="{{ asset('admin/admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
 <!-- iCheck -->
 <link rel="stylesheet" href="{{ asset('admin/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
 <!-- Select2 -->
 <link rel="stylesheet" href="{{ asset('admin/admin-lte/plugins/select2/css/select2.min.css') }}">
-<link rel="stylesheet" href="{{ asset('admin/admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+<link rel="stylesheet"
+    href="{{ asset('admin/admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
 <!-- SweetAlert2 -->
 <link rel="stylesheet" href="{{ asset('admin/admin-lte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
 <!-- Toastr -->
