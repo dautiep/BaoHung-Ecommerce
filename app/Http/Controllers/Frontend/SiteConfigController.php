f2d718d5a4aed8601e35d011a6acddd43f4642d2
duonghoangfurucrm <121424180+duonghoangfurucrm@users.noreply.github.com>
2023-03-04 14:24:36 +0700

    Init categories

--- app/Http/Controllers/Frontend/SiteConfigController.php
+++ app/Http/Controllers/Frontend/SiteConfigController.php
@@ -37,6 +37,39 @@ class SiteConfigController extends Controller
         ];
     }
 
+    public function loadConfigurationCategories()
+    {
+        return [
+            'name_category' => 'Loại sản phẩm',
+            'child_category' => [
+                [
+                    'name_category' => 'Sản phẩm A',
+                    'router_category' => route($this->_prefix_router . 'contact'),
+                    'name_router' => $this->_prefix_router . 'contact',
+                    'status_category' => true,
+                    'child_category' => [
+                        [
+                            'name_category' => 'Sản phẩm loại',
+                            'router_category' => route($this->_prefix_router . 'contact'),
+                            'name_router' => $this->_prefix_router . 'contact',
+                            'status_category' => true,
+                        ]
+                    ],
+                ],
+                [
+                    'name_category' => 'Sản phẩm B',
+                    'router_category' => route($this->_prefix_router . 'contact'),
+                    'name_router' => $this->_prefix_router . 'contact',
+                    'status_page' => true,
+                    'status_category' => true,
+
+                    'child_category' => []
+                ]
+            ],
+
+        ];
+    }
+
     public function hasActiveRoutePage($route_name, $className = 'active')
     {
         return $route_name === request()->route()->getName() ? $className : '';
