5fc4e28f0fbf631cedb55b64c55953e31bd59b85
duonghoangfurucrm <121424180+duonghoangfurucrm@users.noreply.github.com>
2023-03-06 23:05:55 +0700

    script responsive

--- resources/views/frontend/templates/eshopper-1-0-0/layout/layout.blade.php
+++ resources/views/frontend/templates/eshopper-1-0-0/layout/layout.blade.php
@@ -24,12 +24,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <style>
-
         .image_product {
-            height: 500px;
-            width: 500px;
+            object-fit: cover;
         }
-
     </style>
     @yield('css')
 
@@ -43,7 +40,47 @@
     @include(bladeAsset('components.scroll_top'))
     @include(bladeAsset('scripts.scripts'))
     @yield('scripts')
+    <script>
+        $(document).ready(function() {
+            // get the img element
+            const queryAllImg = document.querySelectorAll('img');
+            if (queryAllImg.length > 0) {
 
+            }
+            const closestDiv = queryAllImg[0].closest('div');
+
+            const eachImage = function(array, callback, scope) {
+                for (var i = 0; i < array.length; i++) {
+                    callback.call(scope, i, array[i]);
+                }
+            };
+            eachImage(queryAllImg, function(index, img) {
+
+                // create a new SVG element
+                var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+                // set attributes for the SVG element
+                svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+                svg.setAttribute('width', closestDiv.offsetHeight);
+                svg.setAttribute('height', closestDiv.offsetHeight);
+
+                // create an image element inside the SVG element
+                var svgImg = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+                svgImg.setAttribute('x', 0);
+                svgImg.setAttribute('y', 0);
+                svgImg.setAttribute('width', closestDiv.offsetHeight);
+                svgImg.setAttribute('height', closestDiv.offsetHeight);
+                svgImg.setAttributeNS('http://www.w3.org/1999/xlink', 'href', img.src);
+
+                // add the image element to the SVG element
+                svg.appendChild(svgImg);
+
+                // replace the img element with the new SVG element
+                img.parentNode.replaceChild(svg, img);
+            });
+
+        });
+    </script>
 </body>
 
 </html>
