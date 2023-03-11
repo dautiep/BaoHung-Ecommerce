22dac18a785498d1734c9eb5e8cdc15136a29dd6
duonghoangfurucrm <121424180+duonghoangfurucrm@users.noreply.github.com>
2023-03-11 23:43:14 +0700

    add text truncate search result

--- resources/views/frontend/templates/eshopper-1-0-0/components/search_item.blade.php
+++ resources/views/frontend/templates/eshopper-1-0-0/components/search_item.blade.php
@@ -1,7 +1,7 @@
     <a class="dropdown-item" href="#"> <i class="fa fa-search"></i> {{ request()->name }}</a>
     @if (@$product_detail)
         @foreach ($product_detail as $product)
-            <a class="dropdown-item"
+            <a class="dropdown-item text-truncate"
                 href="{{ route('frontend.product.detail', ['slug' => @$product->slug]) }}">{{ @$product->name }}</a>
         @endforeach
     @endif
