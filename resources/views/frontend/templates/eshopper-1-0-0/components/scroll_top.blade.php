f1389fdfb8206a48e497ae226acb9b0bb4885621
dautiep <dautiep123@gmail.com>
2023-03-12 17:01:05 +0700

    remove chatbot

--- resources/views/frontend/templates/eshopper-1-0-0/components/scroll_top.blade.php
+++ resources/views/frontend/templates/eshopper-1-0-0/components/scroll_top.blade.php
@@ -1,3 +1,3 @@
 <!-- Back to Top -->
-<a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
-<span class="icon-chat-bot"></span>
+{{-- <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+<span class="icon-chat-bot"></span> --}}
