ff00716771aa391fc63676e7c28a8db87da80aea
duonghoangfurucrm <121424180+duonghoangfurucrm@users.noreply.github.com>
2023-03-13 08:44:33 +0700

    update search_item

--- resources/views/frontend/templates/eshopper-1-0-0/components/search_item.blade.php
+++ resources/views/frontend/templates/eshopper-1-0-0/components/search_item.blade.php
@@ -1,4 +1,4 @@
-    <a class="dropdown-item" href="#"> <i class="fa fa-search"></i> {{ request()->name }}</a>
+    <a class="dropdown-item text-truncate" href="#"> <i class="fa fa-search"></i> {{ request()->name }}</a>
     @if (@$product_detail)
         @foreach ($product_detail as $product)
             <a class="dropdown-item text-truncate"
